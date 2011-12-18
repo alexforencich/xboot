f0334aaf56516c4eb68962a4d26b45b3dd727374
Alex Forencich <alex@alexforencich.com>
2011-12-17 23:51:32 -0800

    Updated mkpackage.sh

--- mkpackage.sh
+++ mkpackage.sh
@@ -1,24 +1,37 @@
 #!/bin/bash
 
-cd ..
+files="README
+Makefile
+xboot.c
+xboot.h
+flash.c
+flash.h
+eeprom_driver.c
+eeprom_driver.h
+protocol.h
+sp_driver.S
+sp_driver.h
+uart.c
+uart.h
+i2c.c
+i2c.h
+fifo.c
+fifo.h
+watchdog.c
+watchdog.h
+api.c
+api.h
+xbootapi.c
+xbootapi.h"
 
-output=xboot/xboot-$(date +%Y%m%d).tar.gz
+name=xboot
 
-tar -cvzf $output \
-xboot/README \
-xboot/Makefile \
-xboot/xboot.c \
-xboot/xboot.h \
-xboot/eeprom_driver.c \
-xboot/eeprom_driver.h \
-xboot/fifo.c \
-xboot/fifo.h \
-xboot/i2c.c \
-xboot/i2c.h \
-xboot/protocol.h \
-xboot/sp_driver.S \
-xboot/sp_driver.h \
-xboot/uart.c \
-xboot/uart.h \
-xboot/watchdog.c \
-xboot/watchdog.h
+output=xboot-$(date +%Y%m%d).tar.gz
+
+mkdir -p pkg/$name
+rm -rf pkg/$name/*
+cp -r $files pkg/$name
+
+cd pkg
+tar -cvzf ../$output xboot
+
